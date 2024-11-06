f56c55096db8adaa8f43a9576c201776d6ba234b
Adilfarooque <Adilfarq11@gmail.com>
2024-11-06 09:52:57 +0530

    Updated Move all zero func

--- Leetcode_283.go
+++ Leetcode_283.go
@@ -3,8 +3,12 @@ package main
 
 
 //Move All zero into the end
-func MoveAllzero(zero []int){
-	
+func MoveAllzero(nums []int){
+	nonZero := 0
+    for i := 0; i < len(nums); i++ {
+        nums[nonZero],nums[i] = nums[i], nums[nonZero]
+        nonZero++
+    }
 }
 func main(){
 	
