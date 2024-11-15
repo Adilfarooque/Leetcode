46f372114099b29233bc66a2d098c6f307269303
Adilfarooque <Adilfarq11@gmail.com>
2024-11-15 17:35:28 +0530

    Di String Match

--- main.go
+++ main.go
@@ -21,24 +21,28 @@ func main() {
 
 	//Sorted Squares
 	nums := []int{-4, -1, 0, 3, 10}
-	fmt.Println("Square of sorted array:",SortedSquares(nums))
+	fmt.Println("Square of sorted array:", SortedSquares(nums))
 
 	//Largest Perimeter
-	pNumbers := []int{2,1,2,4,3}
-	fmt.Println("Largest perimeters :",largestPerimeter(pNumbers))
-
+	pNumbers := []int{2, 1, 2, 4, 3}
+	fmt.Println("Largest perimeters :", largestPerimeter(pNumbers))
 
 	//Repeated N Times
-	NTimes := []int{2,1,2,5,3,2}
-	fmt.Println("Repeated elements :",repeatedNTimes(NTimes))
+	NTimes := []int{2, 1, 2, 5, 3, 2}
+	fmt.Println("Repeated elements :", repeatedNTimes(NTimes))
 
 	//Valid mountain Array
-	mountain := []int{0,3,2,1}
-	fmt.Println("Valid mountain :",validMountainArray(mountain))
+	mountain := []int{0, 3, 2, 1}
+	fmt.Println("Valid mountain :", validMountainArray(mountain))
 
 	//Min Deletion Size
-	strs := []string{"cba","daf","ghi"}
-	fmt.Println("Min Deletion Size :",MinDeletionSize(strs))
+	strs := []string{"cba", "daf", "ghi"}
+	fmt.Println("Min Deletion Size :", MinDeletionSize(strs))
+
+	//dIStringMatch
+	s := "III"
+	fmt.Println("DIStringMatch :", diStringMatch(s))
+	s = "IDID"
+	fmt.Println("DIStringMatch :", diStringMatch(s))
 
 }
-
