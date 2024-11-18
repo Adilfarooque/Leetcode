e072d5d2a261075df46d77c97b36a8ac8d0621b7
Adilfarooque <Adilfarq11@gmail.com>
2024-11-18 17:23:33 +0530

    Smallest Range

---
+++ Leetcode_908.go
@@ -0,0 +1,18 @@
+package main
+
+
+func smallestRangeI(nums []int, k int) int {
+    min, max := nums[0], nums[0]
+    for _, v := range nums {
+        if v < min {
+            min = v
+        }
+        if v > max {
+            max = v
+        }
+    }
+    if max-min <= 2*k {
+        return 0
+    }
+    return max-min-2*k 
+}
